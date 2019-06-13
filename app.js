d93cab2049f6ec12f3709d522b863f3b6aecd88f
Mav <mavericktesini@gmail.com>
2019-06-12 21:20:17 -0400

    middleware theory

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const http = require('http')
 
-const server = http.createServer();
-console.log('Server is Listening...')
+const express = require('express')
+
+const app = express();
+
+// three args: req, res, next
+app.use((req, res, next) => {
+  console.log('In the middelware');
+  next()
+});
+app.use((req, res, next) => {
+  console.log('Second middleware');
+});
+
+const server = http.createServer(app);
+
 server.listen(3000);
