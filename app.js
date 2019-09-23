7179dccb8ec339e78cf082ebb044151d6c5f7f2a
Mav <mavericktesini@gmail.com>
2019-09-23 19:58:34 -0400

    replace notFound with 404

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const notFoundRoute = require('./routes/not-found');
 
-app.use(bodyParser.urlencoded({extended: false}))
+// middlewares fire in sequence, so the catchall should be last and non-routes shall be first
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(shopRoutes)
-app.use('/admin', adminRoutes)
-app.use(notFoundRoute)
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(notFoundRoute);
 
-app.listen(3000)
+app.listen(3000);
--- routes/not-found.js
+++ routes/not-found.js
@@ -1,11 +1,11 @@
-const path = require('path'); 
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 const rootDir = require('../utils/path');
 
 router.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', 'not-found.html'));
-});
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+})
 
 module.exports = router;
