71fcbce745110dae1e723b24b3b9787a2232843a
Mav <mavericktesini@gmail.com>
2019-09-25 21:39:01 -0400

    add static mw and convert style tags to links

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -8,6 +9,8 @@ const notFoundRoute = require('./routes/not-found');
 
 // middlewares fire in sequence, so the catchall should be last and non-routes shall be first
 app.use(bodyParser.urlencoded({ extended: false }));
+// serve static read access
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(shopRoutes);
 app.use('/admin', adminRoutes);
