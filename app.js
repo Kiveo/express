c9a18d9872d1576db371966023338850c3e48d1f
Mav <mavericktesini@gmail.com>
2019-09-29 14:25:27 -0400

    refactor to use ejs view engine and apply to 404

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-
+// routes // 
 const adminData = require('./routes/admin');
-
 const shopRoutes = require('./routes/shop');
 const notFoundRoute = require('./routes/not-found');
 
-// middlewares fire in sequence, so the catchall should be last and non-routes shall be first
-app.use(bodyParser.urlencoded({ extended: false }));
-// serve static read access
-app.use(express.static(path.join(__dirname, 'public')));
+// middlewares fire in sequence, so the catch-all should be last and non-routes shall be first
+app.set('view engine', 'ejs'); // ejs used within views for data passing
+app.use(bodyParser.urlencoded({ extended: false })); // form parser
+app.use(express.static(path.join(__dirname, 'public'))); // serve static read access
 
 app.use(shopRoutes);
 app.use('/admin', adminData.routes);
--- routes/not-found.js
+++ routes/not-found.js
@@ -5,7 +5,8 @@ const router = express.Router();
 const rootDir = require('../utils/path');
 
 router.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
 })
 
 module.exports = router;
