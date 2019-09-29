7ce272ae3ea9fe15090bc83664a113b76895ca86
Mav <mavericktesini@gmail.com>
2019-09-29 14:33:22 -0400

    refactor to use ejs file for add-product

--- routes/not-found.js
+++ routes/not-found.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const rootDir = require('../utils/path');
 
 router.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  // render('viewName', data)
   res.status(404).render('404', { pageTitle: 'Page Not Found' })
 })
 
