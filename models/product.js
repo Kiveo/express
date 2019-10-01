587e3f81be2da499f595583560e2082d58097bff
Mav <mavericktesini@gmail.com>
2019-09-30 22:14:55 -0400

    add file for products storage, adjust model to store it

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs'); // access file system
+const path = require('path'); // use paths across diff os
 
 module.exports = class Product {
   constructor(t) {
@@ -6,13 +7,28 @@ module.exports = class Product {
   }
 
   save() {
-    // add "this" instance to products array
-    products.push(this);
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log("::file error::", err)
+      });
+    });
   }
 
   // can be called on class directly (static)
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    })
   }
 
 }
